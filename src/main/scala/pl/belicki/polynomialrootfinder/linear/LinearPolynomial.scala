1ba12636154581c503ffa799eb8f0c4027e89d11
Marcin Belicki <marcin.belicki@protonmail.com>
2024-04-28 18:24:49 +0200

    added Linear

---
+++ src/main/scala/pl/belicki/polynomialrootfinder/linear/LinearPolynomial.scala
@@ -0,0 +1,8 @@
+package pl.belicki.polynomialrootfinder.linear
+
+import pl.belicki.polynomialrootfinder.generic.Polynomial
+
+case class LinearPolynomial(
+    a: Double,
+    b: Double
+) extends Polynomial
