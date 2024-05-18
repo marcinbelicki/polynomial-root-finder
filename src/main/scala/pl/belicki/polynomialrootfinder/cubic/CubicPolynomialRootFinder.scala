ae063aa8a576abf42790d15c83b012cd1bf7c6e5
Marcin Belicki <marcin.belicki@protonmail.com>
2024-05-18 16:13:28 +0200

    added Cubic

--- src/main/scala/pl/belicki/polynomialrootfinder/cubic/CubicPolynomialRootFinder.scala
+++ src/main/scala/pl/belicki/polynomialrootfinder/cubic/CubicPolynomialRootFinder.scala
@@ -6,7 +6,18 @@ import pl.belicki.polynomialrootfinder.quadratic.QuadraticPolynomialRootFinder
 
 object CubicPolynomialRootFinder extends PolynomialRootFinder[CubicPolynomial, QuadraticPolynomial] {
 
-  override protected def findRootsIfLegit(polynomial: CubicPolynomial): List[Double] = ???
+  override protected def findRootsIfLegit(polynomial: CubicPolynomial): List[Double] = {
+    val A = ???
+    val phi = ???
+    val sigma = ???
+    val B = ???
+    val tau = ???
+    val R3minus = ???
+    val b = ???
+    val R3plus = ???
+
+    ???
+  }
 
   override protected def fallbackFinder: PolynomialRootFinder[QuadraticPolynomial, _] = QuadraticPolynomialRootFinder
 
