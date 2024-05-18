e01ab2efb9281b657fbc9e94933c2c4c0d947f5e
Marcin Belicki <marcin.belicki@protonmail.com>
2024-05-18 16:14:43 +0200

    added source

--- src/main/scala/pl/belicki/polynomialrootfinder/cubic/CubicPolynomialRootFinder.scala
+++ src/main/scala/pl/belicki/polynomialrootfinder/cubic/CubicPolynomialRootFinder.scala
@@ -6,6 +6,7 @@ import pl.belicki.polynomialrootfinder.quadratic.QuadraticPolynomialRootFinder
 
 object CubicPolynomialRootFinder extends PolynomialRootFinder[CubicPolynomial, QuadraticPolynomial] {
 
+  // https://community.ptc.com/sejnu66972/attachments/sejnu66972/PTCMathcad/103386/1/Article%20-On%20formulae%20for%20roots%20of%20cubic%20equation-.pdf
   override protected def findRootsIfLegit(polynomial: CubicPolynomial): List[Double] = {
     val A = ???
     val phi = ???
