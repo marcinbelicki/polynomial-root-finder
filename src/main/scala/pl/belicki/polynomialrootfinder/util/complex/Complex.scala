f32968b91584caa290f32f7ff2404c14ea9d6087
Marcin Belicki <marcin.belicki@protonmail.com>
2024-06-09 22:31:18 +0200

    added complex methods

--- src/main/scala/pl/belicki/polynomialrootfinder/util/complex/Complex.scala
+++ src/main/scala/pl/belicki/polynomialrootfinder/util/complex/Complex.scala
@@ -1,6 +1,130 @@
 package pl.belicki.polynomialrootfinder.util.complex
 
 case class Complex(
-  x: Double,
-  y: Double
-)
+    x: Double,
+    y: Double
+) {
+  def +(that: Complex): Complex = Complex(x + that.x, y + that.y)
+  def -(that: Complex): Complex = Complex(x - that.x, y - that.y)
+
+  def *(that: Complex): Complex =
+    Complex(x * that.x - y * that.y, x * that.y + y * that.x)
+
+  def /(that: Complex): Complex = {
+    require(that.x != 0 || that.y != 0)
+
+    val denominator = Math.pow(that.x, 2) + Math.pow(that.y, 2)
+
+    val realPartNumerator = x * that.x + y * that.y
+    val imaginaryPartNumerator = y * that.x - x * that.y
+
+    Complex(
+      x = realPartNumerator / denominator,
+      y = imaginaryPartNumerator / denominator
+    )
+  }
+
+  def +[NUM: Numeric](that: NUM): Complex = {
+    val numeric = implicitly[Numeric[NUM]]
+
+    val double = numeric.toDouble(that)
+
+    copy(x = x + double)
+  }
+
+  def -[NUM: Numeric](that: NUM): Complex = {
+    val numeric = implicitly[Numeric[NUM]]
+
+    val double = numeric.toDouble(that)
+
+    copy(x = x - double)
+  }
+
+  def *[NUM: Numeric](that: NUM): Complex = {
+    val numeric = implicitly[Numeric[NUM]]
+
+    val double = numeric.toDouble(that)
+
+    Complex(
+      x = x * double,
+      y = y * double
+    )
+  }
+
+  def /[NUM: Numeric](that: NUM): Complex = {
+    val numeric = implicitly[Numeric[NUM]]
+    require(that != numeric.zero)
+
+    val double = numeric.toDouble(that)
+
+    Complex(
+      x = x / double,
+      y = y / double
+    )
+  }
+}
+
+object Complex {
+
+  def arg(complex: Complex): Double = Math.atan2(complex.y, complex.x)
+
+  def abs(complex: Complex): Double =
+    Math.sqrt(Math.pow(complex.x, 2) + Math.pow(complex.y, 2))
+
+  def exp(complex: Complex): Complex = {
+    val realPartExp = Math.exp(complex.x)
+
+    exponential(realPartExp, complex.y)
+  }
+
+  def expImaginary(imaginary: Double): Complex =
+    Complex(
+      x = Math.cos(imaginary),
+      y = Math.sin(imaginary)
+    )
+
+  def real(complex: Complex): Double = complex.x
+
+  def imaginary(complex: Complex): Double = complex.y
+
+  def pow(complex: Complex, power: Int) = {
+    val abs = Math.pow(this.abs(complex), power)
+    val arg = this.arg(complex) * power
+
+    exponential(abs, arg)
+  }
+
+  def exponential(abs: Double, arg: Double): Complex =
+    Complex(
+      x = Math.cos(arg) * abs,
+      y = Math.sin(arg) * abs
+    )
+
+  implicit class RealAsComplex[NUM: Numeric](real: NUM) {
+    private def numeric = implicitly[Numeric[NUM]]
+    private def double = numeric.toDouble(real)
+
+    def +(complex: Complex): Complex = complex.copy(x = complex.x + double)
+
+    def -(complex: Complex): Complex = Complex(
+      x = double - complex.x,
+      y = -complex.y
+    )
+
+    def *(complex: Complex): Complex = complex * real
+
+    def /(complex: Complex): Complex = {
+      require(complex.x != 0 || complex.y != 0)
+
+      val denominator = Math.pow(complex.x, 2) + Math.pow(complex.y, 2)
+
+      val multiplier = double / denominator
+
+      Complex(
+        x = complex.x * multiplier,
+        y = complex.y * multiplier
+      )
+    }
+
+  }
+}
